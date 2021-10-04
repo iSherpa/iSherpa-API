ac8ff94e6d6c34fbff059252701c6d114d70d1bc
jones-trae <jones.trae@gmail.com>
2021-10-04 14:40:14 -0700

    init query

--- modules/Hike.js
+++ modules/Hike.js
@@ -1,7 +1,25 @@
 'use strict'
+const axios = require('axios');
 
-const mongoose = require('mongoose')
-const {Schema} = mongoose
 
-// parData get() will go here. needs to call a function. 
-class parkData {};
+
+
+
+
+// getparkData get() will go here. needs to call a function. is a query calls park API and gets data by state code
+  
+  async function getParkData(request, response) {
+    const stateCode = request.query.stateCode
+    const url = `https://developer.nps.gov/api/v1/parks?stateCode=${stateCode}&key=${process.env.API_KEY}`
+      const Data = await axios.get(url)
+      response.send(Data.data)
+      
+  }
+
+  
+
+
+
+
+
+module.exports={getParkData};
