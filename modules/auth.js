eb9e83b002c5266f7f3dbe4643e5e147b319c1e2
Jacob Gregor <67080552+JacobGregor@users.noreply.github.com>
2021-10-04 22:20:09 -0700

    delete button done

---
+++ modules/auth.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken'); // auth
+const jwksClient = require('jwks-rsa'); // auth
+
+// this comes directly from the jsonwebtoken docs
+const client = jwksClient({
+// this url comes from your app on the auth0 dashboard ------- NEED TO GET THIS AND SET IT ON MY BACKEND..
+  jwksUri: process.env.JWKS_URI
+});
+
+// this function comes directly from the jsonwebtoken docs
+function getKey(header, callback) {
+  client.getSigningKey(header.kid, function (err, key) {
+    const signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+}
+
+function verifyUser(request, errOrUserCallback) {
+
+  try {
+    const token = request.headers.authorization.split(' ')[1];
+
+    jwt.verify(token, getKey, {}, errOrUserCallback);
+  } catch (error) {
+    errOrUserCallback('Not Authorized');
+  }
+}
+
+console,log(token)
+
+
+module.exports = {verifyUser}
